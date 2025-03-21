a131e0661c364166d3ed1152b3e44b4a04acf0b0
guniism <gun16110@hotmail.com>
2025-03-21 21:21:43 +0700

    Finish

--- src/app/booking/page.tsx
+++ src/app/booking/page.tsx
@@ -1,11 +1,39 @@
 import DateReserve from "@/components/DateReserve";
 import { Select, MenuItem, TextField } from "@mui/material";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../api/auth/[...nextauth]/authOptions";
+import getUserProfile from "@/libs/getUserProfile";
 
-export default function BookingPage() {
+export default async function BookingPage() {
+    const session = await getServerSession(authOptions);
+
+    if(!session || !session.user.token) return null;
+
+    const profile = await getUserProfile(session.user.token);
+    var createdAt = new Date(profile.data.createdAt);
+
     return (
     <main>
         <h1 className='text-3xl font-bold text-center mt-10 mb-5'>Venue Booking</h1>
         
+        <div className="text-2xl text-center">{profile.data.name}</div>
+        <table className="table-auto border-separate border-spacing-2 mx-auto">
+            <tbody>
+                <tr>
+                <td>Email</td>
+                <td>{profile.data.email}</td>
+                </tr>
+                <tr>
+                <td>Tel.</td>
+                <td>{profile.data.tel}</td>
+                </tr>
+                <tr>
+                <td>Member Since</td>
+                <td>{createdAt.toString()}</td>
+                </tr>
+            </tbody>
+        </table>
+
         <form className="flex flex-col max-w-md mx-auto space-y-5 p-5">  
             <TextField variant="standard" name="Name-Lastname" label="Name-Lastname" placeholder="Enter your name"></TextField>
             <TextField variant="standard" name="Contact-Number" label="Contact-Number" placeholder="Enter contact number"></TextField>
--- src/components/TopMenu.tsx
+++ src/components/TopMenu.tsx
@@ -1,10 +1,30 @@
 import TopMenuItem from './TopMenuItem'
 import Image from 'next/image'
+import { getServerSession } from 'next-auth'
+import { authOptions } from '@/app/api/auth/[...nextauth]/authOptions'
+import Link from 'next/link'
 
-export default function TopMenu(){
+export default async function TopMenu(){
+    const session = await getServerSession(authOptions );
+
     return(
         <div className="bg-white shadow-sm">
-            {/* <div className="max-w-6xl w-full mx-auto px-4"> */}
+            <div className="w-full mx-auto px-2 flex justify-between">
+                {
+                    session ? <Link href="/api/auth/signout" className='flex items-center text-[#055D70] hover:text-[#0d7c94]'>
+                        <div>
+                            <b className="font-semibold ">Sign-Out of {session.user.name}</b>
+                        </div>
+                    </Link> 
+                    : <Link href="/api/auth/signin" className='flex items-center text-[#055D70] hover:text-[#0d7c94]'>
+                        <div>
+                            <b className="font-semibold ">Sign-In</b>
+                        </div>
+                    </Link>
+
+                }
+
+
                 <div className="h-14 flex flex-row-reverse ">
                     <div className="px-1 h-full flex items-center justify-center">
                         <Image src="/img/logo.png"
@@ -16,7 +36,7 @@ export default function TopMenu(){
                     </div>
                     <TopMenuItem menuName="Booking" link="/booking"/>
                 </div>
-            {/* </div> */}
+            </div>
         </div>
     )
 }
